5b70d6cc8a807fae9fd508fd3ac1db50859ab566
Mirel <54960567+MirelCP@users.noreply.github.com>
2022-05-29 17:17:45 +0200

    Delete factorial-while.js

--- factorial-while.js
+++
@@ -1,9 +0,0 @@
-//Calcular el factorial usando un bucle while
- let num = 10;
-    let factorial = 1;
-    let i = 1;
-    while(i<=num){
-        factorial = factorial * i;
-        i++;
-    }
-    console.log(factorial);
